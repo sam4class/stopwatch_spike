5c5f053ecb55e7802b2ff7874daf61140caeaf85
Samantha Marlowe <sam.c.marlowe@gmail.com>
2023-05-09 13:54:22 -0500

    spiked the timer and am getting min and sec

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { useDispatch } from "react-redux"; 
+
 
 function App() {
+  const dispatch = useDispatch();
+
+  const [time, setTime] = useState(0);
+  const [timerOn, setTimerOn] = useState(false);
+  const [stoppedTime, setStoppedTime] = useState(null);
+
+  useEffect(() => {
+    let interval = null;
+
+    if(timerOn) {
+  interval = setInterval(() => {
+    setTime(prevTime => prevTime + 10)
+  }, 10)
+}else{
+  clearInterval(interval);
+  setStoppedTime(time);
+}
+
+return () => clearInterval(interval)
+  }, [timerOn, time])
+
+const handleSubmit = (event) => {
+  const minutes = Math.floor(time / 60000);
+  const seconds = ((time % 60000) / 1000).toFixed(0);
+  console.log('HERERERRE', minutes, seconds)
+  setStoppedTime(minutes + ":" + (seconds < 10 ? '0' : '') + seconds)
+  console.log('this is in the handleStop', minutes + ":" + (seconds < 10 ? '0' : '') + seconds)
+
+  dispatch ({
+    type: 'POST_TIME',
+    payload: minutes + ":" + (seconds < 10 ? '0' : '') + seconds
+  })
+  console.log('HERE is stoppedtime', minutes + ":" + (seconds < 10 ? '0' : '') + seconds)
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="timer">
+      <span className="digits">{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+      <span className="digits">{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+      <span className="mili-sec">{("0" + ((time / 10) % 100)).slice(-2)}</span>
+      </div>
+      <div>
+        {!timerOn && time === 0 && (
+        <button className="btn" onClick={() => setTimerOn(true)}>Start</button>
+        )}
+        {timerOn && (
+        <button className="btn" onClick={() => setTimerOn(false)}>Stop</button>
+        )}
+        {!timerOn && time !== 0 && (
+        <button className="btn" onClick={() => setTimerOn(true)}>Resume</button>
+        )}
+        {!timerOn && time > 0 && (
+        <button className="btn" onClick={() => {setTime(0); setStoppedTime(null);}}>Reset</button>
+        )}
+      </div>
+      <div>
+       
+        <button className="submitBtn" onClick={handleSubmit}>Submit Time</button>
+        
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,44 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put } from 'redux-saga/effects';
+import axios from 'axios';
+
+const sagaMiddleware = createSagaMiddleware();
+
+function* postTimerOutput(action){
+  console.log('this is in our saga', action.payload)
+  try{
+    const timer = yield axios.post('api/time', action.payload)
+    yield put({type: 'POST_TIME', payload: timer.data})
+  }catch (err){
+    console.log('error in POST saga', err)
+  }
+}
+
+function* rootSaga(){
+  console.log('inside rootSaga()')
+  yield takeEvery('POST_TIME', postTimerOutput)
+  
+}
+
+const store = createStore(
+  combineReducers({}),
+  applyMiddleware(sagaMiddleware)
+)
+
+sagaMiddleware.run(rootSaga);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
